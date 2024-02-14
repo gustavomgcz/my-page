9c53a8ea619cb3fd8373bbe870817eb27a806b7e
Gustavo Magalhaes <117767677+gustavomgcz@users.noreply.github.com>
2024-02-14 18:15:07 -0300

    Add files via upload
    Atualização Imgs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-const cursorTibia = document.querySelector('#tibia')
+const gifsTibia = [
+    '../img/Assassin Male.gif',
+    './img/Barbarian Male.gif',
+    './img/Beggar Male.gif',
+    './img/Brotherhood Female.gif',
+    './img/Brotherhood Male.gif',
+    './img/Citizen Male.gif',
+    './img/Druid Male.gif',
+    './img/Hunter Male.gif',
+    './img/Jester Male.gif',
+    './img/Knight Male.gif',
+    './img/Oriental Male.gif',
+    './img/Pirate Male.gif',
+    './img/Shaman Male.gif',
+    './img/Summoner Male.gif',
+    './img/Warrior Male.gif',
+    './img/Wizard Male.gif'
+]
+
+const cursorTibia = document.querySelectorAll('.tibia')
 const imagemTibia = document.querySelector('.sobre__imagem--tibia')
 
-cursorTibia.addEventListener("mouseover", function() {
-    imagemTibia.style.display = "inline"
-})
+cursorTibia.forEach(elemento => {
+   
+    elemento.addEventListener("mouseover", function() {
+        const aleatorio = Math.floor(Math.random() * 16)
+        imagemTibia.src = gifsTibia[aleatorio]
+        imagemTibia.style.display = "inline"
+    })
 
-cursorTibia.addEventListener("mouseout", function() {
-    imagemTibia.style.display = "none"
-})
+    elemento.addEventListener("mouseout", function() {
+        imagemTibia.src = ""
+        imagemTibia.style.display = "none"
+    })
+
+});
